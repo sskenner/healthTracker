88bd6904e5551104c7cb9ec00cc94aa9d6689a88
sskenner <sskenner@gmail.com>
2016-09-04 14:28:10 +0000

    update display & action functionality

--- js/app.js
+++ js/app.js
@@ -8,16 +8,13 @@ window.App = {
   Views: {}
 };
 
- //? _.template($('#' + id).html());
-
-// Define Food Model
+// Define Model of Food
 App.Models.Food = Backbone.Model.extend({
-  defaults: {
+  default: {
     name: '',
     calories: 0
   },
 
-  //? is this necessary due to api validation? .. see notes
   // Ensure that each food item has a name
   validate: function(foodItem) {
     if(! $.trim(foodItem.name))  {
@@ -26,50 +23,62 @@ App.Models.Food = Backbone.Model.extend({
   }
 });
 
-// Define (Foods Model) Collection
+// Define Collection of Foods
 App.Collections.Foods = Backbone.Collection.extend({
-  model: App.Models.Food
+  model: App.Models.Food,
+  // Implement local storage persistence of browser
+  localStorage: new Backbone.LocalStorage('LocalFoods')
 });
 
-// Create (Food Model) View
+// Create view and actions to remove items from list of selected food
 App.Views.Food = Backbone.View.extend({
   tagName: 'li',
   className: 'selectedItem',
 
-//? is ok to pass this?
-  template: _.template($('.foodListTemplate').html()),
+  // Template call for list display
+  template: _.template($('#foodListTemplate').html()),
 
+  // Setup event handler to call destroy method
   initialize: function() {
     this.model.on('destroy', this.remove, this);
   },
 
+  // Enable food deletion by binding destroy method to click event
   events:  {
     'click .delete' : 'destroy'
   },
-
+  // Create view via template of JSON parsed model
   render: function() {
     var template = this.template(this.model.toJSON());
+    this.$el.html(template);
+    return this;
   },
 
+  //
   destroy: function() {
     this.model.destroy();
   },
 
   remove: function() {
+    // Show alert if there is no food in list
+    if (this.$el.siblings().lenght === 0) {
+      $('#searchAlert').show();
+    }
     this.$el.remove();
   }
 });
 
-// Create (Foods) View
+// Create view and actions to add items to list of selected food
 App.Views.Foods = Backbone.View.extend({
   tagName: 'ul',
+  className: 'selectedResult',
 
   // Setup event handler to call addOne method
   initialize: function() {
     this.collection.on('add', this.addOne, this);
   },
 
-  // Loop through and render unordered list of selected food objects
+  // Iterate to render unordered list of selected food
   render: function() {
     this.collection.each(this.addOne, this);
     return this;
@@ -77,7 +86,8 @@ App.Views.Foods = Backbone.View.extend({
 
   // Create and append a food DOM element
   addOne: function(item) {
-    var foodsView = new App.Views.Foods({ model: item });
+    $('#searchAlert').hide();
+    var foodsView = new App.Views.Food({ model: item });
     this.$el.append(foodsView.render().el);
   }
 });
@@ -85,23 +95,25 @@ App.Views.Foods = Backbone.View.extend({
 
 // Create (list and add foods) View
 App.Views.AddFoods = Backbone.View.extend({
-  el: '#addFood',
+  el: '#addItem',
 
   events: {
-    'click #foodSubmit' : 'submit'
+    'click #itemSubmit' : 'submit'
   },
 
   submit: function(e) {
     e.preventDefault();
-    var foodNames = $('#foodNames').text().toString();
-    var foodCalories = parseInt($('#foodCalories').text());
+    var foodNames = $('#foodName').text().toString();
+    var foodCalories = parseInt($('#foodCalorie').text());
 
     if (isNaN(foodCalories)) {
       return;
     }
 
-    var food = new App.Models.Food({ name: foodNames, calories: foodCalories }, {validate: true});
-    this.collection.add(food);
+    var item = new App.Models.Food({ name: foodNames, calories: foodCalories }, {validate: true});
+    this.collection.add(item);
+    // Add to localstorage
+    item.save();
   }
 });
 
@@ -116,12 +128,12 @@ App.Views.TotCals = Backbone.View.extend({
   },
 
   render: function() {
-    var total = 0;
-    this.collection.each(function(item) {
-      total += parseInt(item.get('calories'));
+    var totCals = 0;
+    this.collection.each(function(obj) {
+      totCals += parseInt(obj.get('calories'));
     }, this);
 
-    this.$el.text(total);
+    this.$el.text(totCals);
 
     return this;
   }
@@ -145,16 +157,16 @@ App.Views.SearchResult = Backbone.View.extend({
         return;
       }
       self.hooks.searchBoxAlert.text('');
-      self.viaAPI(query);
+      self.getFood(query);
     });
   },
 
-  viaAPI: function(query) {
+  getFood: function(query) {
     var self = this;
 //? see notes
     var searchResults = $('.searchResults');
 
-    var searchResultshtml('<p>loading...</p>');
+    searchResults.html('<p>loading...</p>');
 
     $.ajax({
       type: 'GET',
@@ -163,29 +175,31 @@ App.Views.SearchResult = Backbone.View.extend({
 //? see notes
       url: 'https://api.nutritionix.com/v1_1/search/' + query +'?results=0%3A10&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Cnf_calories&appId=b207a370&appKey=16f9bcdd52a20a8e547a5337c4d98b89'}).done(function(data) {
 //? see notes
-        var food;
-        var addButton = $('#foodSubmit');
+        var item;
+        var addButton = $('#itemSubmit');
         var searchItemHTML = '';
-        if(data.hits.length == 0) {
+        if(data.hits.length <= 0) {
           var noResults = '<p>No results found for search term of: ' + query + '</p>';
           searchResults.html(noResults);
           return;
         }
 
         for (var i = 0; i < data.hits.length; i++) {
-          searchItemHTML += '<li class="searchItem"><span class="searchName">' + data.hits[i].fields.brand_name + '</span><span class="searchCalories">' + Math.round(data.hits[i].fields.nf_calories) + '</span></li>';
+          searchItemHTML += '<li class="searchItem"><span class="searchName">' + data.hits[i].fields.item_name + '</span><span class="searchCalories">' + Math.round(data.hits[i].fields.nf_calories) + ' Cal. </span></li>';
         }
         searchResults.html(searchItemHTML);
         var searchItem = $('.searchItem');
         searchItem.on('click', function() {
 //? see notes
           addButton.prop('disabled', false);
-          var names = $(this).find('.searchName').text();
-          var calories = $(this).find('.searchCalories').text();
-          $('#foodNames').text(names);
-          $('#foodCalories').text(calories);
+          var items = $(this).find('.searchName').text();
+          var cals = $(this).find('.searchCalories').text();
+          $('#foodName').text(items);
+          $('#foodCalorie').text(cals);
           return;
         });
+      }).fail(function() {
+        searchResults.html('<p> Oh snap... error retrieving the food information.');
       });
     }
 });
@@ -194,10 +208,23 @@ App.Views.SearchResult = Backbone.View.extend({
 // Initialize App
 //? see notes
 var foodsCollection = new App.Collections.Foods([]);
-var addFoodsView = new App.Views.AddFoods({ collection: foodsCollection });
-var listFoodsView = new App.Views.Foods({ collection: foodsCollection });
-var totCalsView = new App.Views.TotCals({ collection: foodsCollection});
+
+foodsCollection.fetch();
+
+var addFoodView = new App.Views.AddFoods({ collection: foodsCollection });
+
+var listFoods = new App.Views.Foods({ collection: foodsCollection });
+
+var totalFoodCal = new App.Views.TotCals({ collection: foodsCollection});
 
 var searchResult = new App.Views.SearchResult();
 
-$('.foodsList').html(listFoodsView.render().el);
+$('.foodsList').html(listFoods.render().el);
+
+
+
+
+
+
+
+
